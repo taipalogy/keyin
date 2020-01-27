39d933e108338ded978ddae1997f7a6e498002ce
Wessen Sotron <sjhuaii@gmail.com>
2020-01-27 11:09:29 +0800

    Deploy Next.js to keyin

--- docs/_next/static/r91liK7cbZniENMptpsAA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/convnounadv":[a],"/kana":[a],"/word":[a]}}("static\u002Fchunks\u002F6aed3a51fcc5ab514b81e86ca7c32bbff3801171.90ac5a2ca63206ef483d.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
