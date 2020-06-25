03638ee56432326f95739e30992e65275ac3ae33
Wessen Sotron <sjhuaii@gmail.com>
2020-06-25 14:56:17 +0800

    input multiple words in taikana page

--- pages/taikana.tsx
+++ pages/taikana.tsx
@@ -3,9 +3,19 @@ import { useState } from 'react';
 
 function TaiKanaPage() {
   const [input, setInput] = useState('');
+
+  const tokens: string[] = [];
+
+  const matchArr = input.match(/\w+/g);
+  if (matchArr) {
+    matchArr.filter(it => it != undefined).map(it => tokens.push(it));
+  }
+
   const cli = new Client();
-  const ta = cli.processTonal(input);
-  const seqs = ta.blockSequences.filter(x => x.length > 0);
+  const tas = tokens.map(it => cli.processTonal(it));
+  const listOfSeqs = tas.map(it =>
+    it.blockSequences.filter(it => it.length > 0)
+  );
 
   const handleChange = function (e: React.ChangeEvent<HTMLInputElement>) {
     setInput(e.target.value);
@@ -18,9 +28,10 @@ function TaiKanaPage() {
         <br />
         <input type="text" value={input} onChange={handleChange} />
       </label>
-      {seqs.map(x => (
+      {listOfSeqs.map(x => (
         <li> {x} </li>
       ))}
+      <li>{listOfSeqs.map(y => y[0]).join('')}</li>
     </div>
   );
 }
