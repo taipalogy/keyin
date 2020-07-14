7ae952c9b6be3930e6ed413af2b6f3cb7171c7d0
Wessen Sotron <sjhuaii@gmail.com>
2020-07-14 20:03:12 +0800

    implement tokenizer

--- pages/tokenizer.tsx
+++ pages/tokenizer.tsx
@@ -10,7 +10,8 @@ let deletedLetter: string = '';
 let previousLength: number = 0;
 let precedingLetters: string[] = [];
 let precedingLetter: string = '';
-let movingForward: boolean = true; // moving backward when false
+let movingForward: boolean = true; // false when moving backward
+let beginOfSyllable = 0;
 
 // the current index and the next one are the boundary of a token
 const indices: number[] = [];
@@ -25,6 +26,7 @@ function TokenizerPage() {
   const kanaSeqs = taKana.blockSequences.filter(x => x.length > 0);
 
   const delimited: Array<string[]> = new Array();
+  const sequences: Array<string[]> = new Array();
 
   const letters = graphAnalyzeTonal(input).map(
     x => x.letter && x.letter.literal
@@ -38,6 +40,8 @@ function TokenizerPage() {
 
   // lastLetter is set before previousLetter
   const lastLetter = letters[letters.length - 1];
+  // const lastSecondLetter =
+  //   letters.length > 1 ? letters[letters.length - 2] : '';
 
   if (previousLength > input.length) {
     movingForward = false;
@@ -51,6 +55,7 @@ function TokenizerPage() {
   precedingLetter = precedingLetters[precedingLetters.length - 2]; // for debugging, logging
 
   let sliced;
+
   if (initialSounds.includes(lastLetter) && movingForward == true) {
     if (initialSounds.includes(precedingLetter)) {
       if (
@@ -82,13 +87,23 @@ function TokenizerPage() {
     indices.pop();
   } else if (movingForward == false && initialSounds.includes(deletedLetter)) {
     // moving backward and removed the pushed index
-    indices.pop();
-    if (
-      initialSounds.includes(lastLetter) &&
-      deletedLetter === TonalLetterTags.ng
-    ) {
-      // in case of letter n. when g is dropped from ng
-      indices.push(characters.length - 1);
+    if (lastLetter === TonalLetterTags.ng) {
+      if (deletedLetter === TonalLetterTags.h) {
+        // initial + ng + h. e.g. sngh, dngh
+        indices.pop();
+      }
+      if (
+        initialSounds.includes(lastLetter) &&
+        deletedLetter === TonalLetterTags.ng &&
+        !initialSounds.includes(precedingLetter)
+      ) {
+        // in case of letter n. when g is dropped from ng
+        indices.push(characters.length - 1);
+      }
+    } else if (!initialSounds.includes(lastLetter)) {
+      // the last letter could be a vowel or tonal
+      // the popped out letter is an initial of the following syllable
+      indices.pop();
     }
   } else if (
     lastLetter === TonalLetterTags.tt &&
@@ -104,16 +119,50 @@ function TokenizerPage() {
 
   if (indices.length > 0) {
     for (let i = 0, j = 1; j < indices.length; i++, j++) {
-      const syl = cli.processTonal(input.slice(indices[i], indices[j]))
+      const seqs = cli.processTonal(input.slice(indices[i], indices[j]))
         .blockSequences;
-      if(syl.length > 0) delimited.push(syl);
+      if (seqs.length > 0 && seqs.filter(it => it.length > 0))
+        delimited.push(seqs);
     }
     if (input.length - indices[indices.length - 1] > 0) {
       // the last syllable, if available
-      const syl = cli.processTonal(
+      const seqs = cli.processTonal(
         input.slice(indices[indices.length - 1], input.length)
       ).blockSequences;
-      if(syl.length > 0) delimited.push(syl);
+      if (seqs.length > 0 && seqs.filter(it => it.length > 0))
+        delimited.push(seqs);
+    }
+  }
+
+  let lastSyllable = '';
+  if (indices.length > 0) {
+    for (let i = 0, j = 1; j < indices.length; i++, j++) {
+      const seqs = cli.processTonal(input.slice(indices[i], indices[j]))
+        .blockSequences;
+      if (seqs.length > 0 && seqs.filter(it => it.length > 0).length > 0) {
+        sequences.push(seqs);
+        beginOfSyllable = j;
+      } else {
+        // handle beginOfSyllable for ambisyllabicity
+      }
+    }
+    if (input.length - indices[indices.length - 1] > 0) {
+      // the last syllable, if available
+      const sliced = input.slice(indices[indices.length - 1], input.length);
+      if (sliced.length == 1 && sliced === TonalLetterTags.h) {
+        // the last syllable is the last element from the delimited plus a final h
+        lastSyllable =
+          input.slice(
+            indices[indices.length - 2],
+            indices[indices.length - 1]
+          ) + input.slice(indices[indices.length - 1]);
+
+        const seqs = cli.processTonal(lastSyllable).blockSequences;
+        if (seqs.length > 0 && seqs.filter(it => it.length > 0)) {
+          sequences.pop(); // pop the one that is not a checked one
+          sequences.push([seqs[0]]); // push the neutral one;
+        }
+      }
     }
   }
 
@@ -137,19 +186,21 @@ function TokenizerPage() {
       {delimited.map(x => (
         <li> {x + '(delimited)'} </li>
       ))}
-      {/* <li>
+      <li>taikana string: {sequences.map(it => it).join('')}</li>
+      {/* <li>last syllable: {lastSyllable}</li>
+      <li>
         indices:
         {indices.map(x => x.toString()).join(',')}
       </li>
       <li>number of initial consonants: {indices.length}</li>
       <li>precedingLetter: {precedingLetter}</li>
-      <li>lettersLength: {letters.length}</li>
       <li>soundSeqsLength: {soundSeqs.length}</li>
       <li>
         sliced:
         {sliced}
       </li>
       <li>characters: {characters.join(', ')}</li>
+      <li>lettersLength: {letters.length}</li>
       <li>letters: {letters.join(', ')}</li>
       <li>last letters: {letters[letters.length - 1]}</li>
       <li>soundSeqs: {soundSeqs}</li>
