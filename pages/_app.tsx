b192390b3e4a9794758493260e7e78b99b1f29a8
Wessen Sotron <sjhuaii@gmail.com>
2020-07-19 14:31:01 +0800

    add type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'semantic-ui-css/semantic.min.css';
+import { AppProps } from 'next/app';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
