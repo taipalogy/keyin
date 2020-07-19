5b2b1dfda485e0a1f6dec6993fc38422ac38e57c
Wessen Sotron <sjhuaii@gmail.com>
2020-07-19 13:51:39 +0800

    edit taikana page

--- pages/taikana.tsx
+++ pages/taikana.tsx
@@ -36,7 +36,36 @@ function TaiKanaPage() {
       <Table celled striped collapsing>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell colSpan="9">中聲</Table.HeaderCell>
+            <Table.HeaderCell colSpan="17">初聲</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>{TonalLetterTags.b.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.c.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.ch.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.d.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.g.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.h.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.j.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.k.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.l.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.m.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.n.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.ng.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.p.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.q.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.s.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.t.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.v.toString()}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+      <Table celled striped collapsing>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell colSpan="28">中聲</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
@@ -51,6 +80,63 @@ function TaiKanaPage() {
             <Table.Cell>{TonalLetterTags.or.toString()}</Table.Cell>
             <Table.Cell>{TonalLetterTags.ir.toString()}</Table.Cell>
             <Table.Cell>{TonalLetterTags.er.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.m.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.n.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.ng.toString()}</Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.a.toString() + TonalLetterTags.i.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.a.toString() + TonalLetterTags.u.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.i.toString() + TonalLetterTags.a.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.i.toString() + TonalLetterTags.e.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.i.toString() + TonalLetterTags.o.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.i.toString() + TonalLetterTags.u.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.i.toString() + TonalLetterTags.ur.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.o.toString() + TonalLetterTags.a.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.o.toString() + TonalLetterTags.e.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.u.toString() + TonalLetterTags.i.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.or.toString() + TonalLetterTags.e.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.ir.toString() + TonalLetterTags.i.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.o.toString() + TonalLetterTags.er.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.i.toString() +
+                TonalLetterTags.a.toString() +
+                TonalLetterTags.i.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.i.toString() +
+                TonalLetterTags.a.toString() +
+                TonalLetterTags.u.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.o.toString() +
+                TonalLetterTags.a.toString() +
+                TonalLetterTags.i.toString()}
+            </Table.Cell>
           </Table.Row>
         </Table.Body>
       </Table>
@@ -64,6 +150,72 @@ function TaiKanaPage() {
         <Table.Body>
           <Table.Row>
             <Table.Cell>{TonalLetterTags.nn.toString()}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+      <Table celled striped collapsing>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell colSpan="26">終聲</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>{TonalLetterTags.p.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.t.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.k.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.h.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.pp.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.tt.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.kk.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.hh.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.b.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.g.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.j.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.l.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.s.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.bb.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.gg.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.jj.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.ll.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.ss.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.m.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.n.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.ng.toString()}</Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.m.toString() + TonalLetterTags.h.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.m.toString() + TonalLetterTags.hh.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.n.toString() + TonalLetterTags.h.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.ng.toString() + TonalLetterTags.h.toString()}
+            </Table.Cell>
+            <Table.Cell>
+              {TonalLetterTags.ng.toString() + TonalLetterTags.hh.toString()}
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+      <Table celled striped collapsing>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell colSpan="9">聲調</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>{TonalLetterTags.f.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.y.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.w.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.x.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.z.toString()}</Table.Cell>
+            <Table.Cell>{TonalLetterTags.xx.toString()}</Table.Cell>
           </Table.Row>
         </Table.Body>
       </Table>
