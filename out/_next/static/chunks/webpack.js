9d7fa10cb19b6f920c353e04ffe76ffa5a2f4865
Wessen Sotron <sjhuaii@gmail.com>
2020-10-31 16:03:45 +0800

    Deploy Next.js to keyin

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8921d72e1685f433d65e";
+/******/ 	var hotCurrentHash = "4fd8c8ad067ebb762084";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
