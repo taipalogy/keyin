2e682a188a57534c598930b4ce43350c6bccd1dd
Wes Sotron <taipalogy@gmail.com>
2023-10-11 10:39:52 +0800

    add fieldset for input method

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,86 +2,93 @@ import Link from 'next/link';
 
 function Home() {
   return (
-    <ul>
-      <li>
-        <a href="word">單語</a>
-      </li>
-      <li>
-        <a href="transfixinflection">transfix inflection</a>
-      </li>
-      <li>
-        <a href="suprafix">suprafix</a>
-      </li>
-      <li>
-        <a href="phrasalverb">phrasal verb</a>
-      </li>
-      <li>
-        <a href="surfaceform">surface form</a>
-      </li>
-      <li>
-        <a href="eadjective">e-adjective</a>
-      </li>
-      <li>
-        <a href="composition">composition</a>
-      </li>
-      <li>
-        <a href="tonepattern">tone pattern</a>
-      </li>
-      <li>
-        <a href="tonepatternwords">tone pattern - words</a>
-      </li>
-      <li>
-        <a href="kana">kana</a>
-      </li>
-      <li>
-        <a href="hint">hint</a>
-      </li>
-      <li>
-        <a href="taikana">taiwanese kana</a>
-      </li>
-      <li>
-        <a href="taizjitt">臺日</a>
-      </li>
-      <li>
-        <a href="jittwtaix">日臺</a>
-      </li>
-      <li>
-        <a href="kuaxay">歌仔</a>
-      </li>
-      <li>
-        <a href="tokenizer">羅馬字 syllable tokenizer</a>
-      </li>
-      <li>
-        <a href="inputcells">input cells</a>
-      </li>
-      <li>
-        <a href="entries">entries</a>
-      </li>
-      <li>
-        <a href="ruby">ruby</a>
-      </li>
-      <li>
-        <a href="inputmethod">input method</a>
-      </li>
-      <li>
-        <a href="multiwordinput">multiword input</a>
-      </li>
-      <li>
-        <a href="rime">rime</a>
-      </li>
-      <li>
-        <a href="alliteration">alliteration</a>
-      </li>
-      <li>
-        <a href="toneless">toneless</a>
-      </li>
-      <li>
-        <a href="inflection">inflection</a>
-      </li>
-      <li>
-        <a href="candidate">candidate</a>
-      </li>
-    </ul>
+    <div>
+      <fieldset>
+        <legend>Input Method</legend>
+        <ul>
+          <li>
+            <a href="inputmethod">input method</a>
+          </li>
+          <li>
+            <a href="multiwordinput">multiword input</a>
+          </li>
+          <li>
+            <a href="candidate">candidate</a>
+          </li>
+        </ul>
+      </fieldset>
+      <ul>
+        <li>
+          <a href="word">單語</a>
+        </li>
+        <li>
+          <a href="transfixinflection">transfix inflection</a>
+        </li>
+        <li>
+          <a href="suprafix">suprafix</a>
+        </li>
+        <li>
+          <a href="phrasalverb">phrasal verb</a>
+        </li>
+        <li>
+          <a href="surfaceform">surface form</a>
+        </li>
+        <li>
+          <a href="eadjective">e-adjective</a>
+        </li>
+        <li>
+          <a href="composition">composition</a>
+        </li>
+        <li>
+          <a href="tonepattern">tone pattern</a>
+        </li>
+        <li>
+          <a href="tonepatternwords">tone pattern - words</a>
+        </li>
+        <li>
+          <a href="kana">kana</a>
+        </li>
+        <li>
+          <a href="hint">hint</a>
+        </li>
+        <li>
+          <a href="taikana">taiwanese kana</a>
+        </li>
+        <li>
+          <a href="taizjitt">臺日</a>
+        </li>
+        <li>
+          <a href="jittwtaix">日臺</a>
+        </li>
+        <li>
+          <a href="kuaxay">歌仔</a>
+        </li>
+        <li>
+          <a href="tokenizer">羅馬字 syllable tokenizer</a>
+        </li>
+        <li>
+          <a href="inputcells">input cells</a>
+        </li>
+        <li>
+          <a href="entries">entries</a>
+        </li>
+        <li>
+          <a href="ruby">ruby</a>
+        </li>
+        <li>
+          <a href="rime">rime</a>
+        </li>
+        <li>
+          <a href="alliteration">alliteration</a>
+        </li>
+        <li>
+          <a href="toneless">toneless</a>
+        </li>
+        <li>
+          <a href="inflection">inflection</a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
